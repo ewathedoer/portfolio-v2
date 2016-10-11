52b227daab0778cb859f3db6d8641037f4fcb87e
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-10-11 23:39:53 +0200

    introduce initial state for portfolio section

--- js/react-script.js
+++ js/react-script.js
@@ -1,4 +1,51 @@
-ReactDOM.render(<p>
-                Hello react
-                </p>
-, document.getElementById("react-container"));
+var SubpageLink = React.createClass({
+  render() {
+    return (
+      <li>
+        <button href={this.props.href} className="btn-wide">
+          {this.props.children}
+        </button>
+      </li>
+    )
+  }
+});
+
+var Subpage = React.createClass({
+  getInitialState: function() {
+    return {
+      pageContent: ''
+    };
+  },
+
+  componentDidMount: function() {
+    this.serverRequest = $.get(this.props.href, function (result) {
+      this.setState({
+        pageContent: result
+      });
+      ReactDOM.render(<ul>
+                  <SubpageLink href="subpages/applications.html">
+                    applications
+                  </SubpageLink>
+                  <SubpageLink href="subpages/websites.html">
+                    websites
+                  </SubpageLink>
+                  <SubpageLink href="subpages/design.html">
+                    design
+                  </SubpageLink>
+                </ul>
+                , document.getElementById("portfolio-subpages"));
+    }.bind(this));
+  },
+  
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+  
+  render() {
+    return (<div dangerouslySetInnerHTML={{__html: this.state.pageContent}}></div>)
+  }
+});
+
+/**/
+
+ReactDOM.render(<Subpage href="subpages/portfolio.html" />, document.getElementById("react-container"));
