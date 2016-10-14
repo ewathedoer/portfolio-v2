f6e51d876c3c6b1e6ca3707b364fce38744a870f
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-10-14 11:50:12 +0200

    add basic content to applications and websites subpages

--- js/react-script.js
+++ js/react-script.js
@@ -16,25 +16,6 @@ var Applications = React.createClass({
         
         <section className="col-desktop-narrow">    
           <section className="wraper flex-center">
-            <div className="wrapper flex-space-around">
-              <div className="col-520">
-                <div className="thumbnail img-portfolio">
-                  <a href="https://s.codepen.io/thedoer/debug/EKbrpO" target="_blank">
-                    <img src="images/portfolio/wiki-viewer.png" alt="wikipedia viewer application" />
-                  </a>
-                </div>
-              </div>
-              <div className="col-1-2 order-first">
-                <h3>Wikipedia Viewer</h3>
-                <p>
-                  Users can search through the extracts of Wikipedia articles and filter which they should read fully.
-                </p>
-                <div className="btn-box">
-                  <a href="http://codepen.io/thedoer/pen/EKbrpO" target="_blank" className="btn-portfolio btn">code</a>
-                  <a href="https://s.codepen.io/thedoer/debug/EKbrpO" target="_blank" className="btn-portfolio btn">live site</a>
-                </div>
-              </div>
-            </div>
             
             <div className="wrapper flex-space-around">
               <div className="col-520">
@@ -47,11 +28,71 @@ var Applications = React.createClass({
               <div className="col-1-2 order-first">
                 <h3>Wikipedia Viewer</h3>
                 <p>
-                  Users can search through the extracts of Wikipedia articles and filter which they should read fully.
+                  The app is directed at young explorers. Users can search the term through the extracts of Wikipedia articles and filter them.
                 </p>
                 <div className="btn-box">
                   <a href="http://codepen.io/thedoer/pen/EKbrpO" target="_blank" className="btn-portfolio btn">code</a>
                   <a href="https://s.codepen.io/thedoer/debug/EKbrpO" target="_blank" className="btn-portfolio btn">live site</a>
+                </div>
+              </div>
+            </div>
+            
+            <div className="wrapper flex-space-around">
+              <div className="col-520">
+                <div className="thumbnail img-portfolio">
+                  <a href="http://theonewhodo.es/pomodoro/" target="_blank">
+                    <img src="images/portfolio/pomodoro.png" alt="pomodoro clock application" />
+                  </a>
+                </div>
+              </div>
+              <div className="col-1-2 order-first">
+                <h3>Pomodoro Clock</h3>
+                <p>
+                  Time management app. Users can customize the time of sessions, declare the goal, tweet their progress and analyze the history of finished cycles.
+                </p>
+                <div className="btn-box">
+                  <a href="https://github.com/ewathedoer/pomodoro-clock" target="_blank" className="btn-portfolio btn">code</a>
+                  <a href="http://theonewhodo.es/pomodoro/" target="_blank" className="btn-portfolio btn">live site</a>
+                </div>
+              </div>
+            </div> 
+            
+            <div className="wrapper flex-space-around">
+              <div className="col-520">
+                <div className="thumbnail img-portfolio">
+                  <a href="http://theonewhodo.es/simon-game/" target="_blank">
+                    <img src="images/portfolio/simon.png" alt="simon game application" />
+                  </a>
+                </div>
+              </div>
+              <div className="col-1-2 order-first">
+                <h3>Simon Game</h3>
+                <p>
+                  The memory game where a user has to repeat the sequence of 20 sounds in order to win. As the game progresses, the number of buttons to be pressed increases.
+                </p>
+                <div className="btn-box">
+                  <a href="https://github.com/ewathedoer/simon-game" target="_blank" className="btn-portfolio btn">code</a>
+                  <a href="http://theonewhodo.es/simon-game/" target="_blank" className="btn-portfolio btn">live site</a>
+                </div>
+              </div>
+            </div> 
+            
+            <div className="wrapper flex-space-around">
+              <div className="col-520">
+                <div className="thumbnail img-portfolio">
+                  <a href="http://theonewhodo.es/tictactoe/" target="_blank">
+                    <img src="images/portfolio/tictactoe.png" alt="tic tac toe application" />
+                  </a>
+                </div>
+              </div>
+              <div className="col-1-2 order-first">
+                <h3>Tic Tac Toe Game</h3>
+                <p>
+                  The game has two modes implemented: a beginner and an advanced player logic. Users can play multiple round and change both mode and symbol.
+                </p>
+                <div className="btn-box">
+                  <a href="https://github.com/ewathedoer/tictactoe" target="_blank" className="btn-portfolio btn">code</a>
+                  <a href="http://theonewhodo.es/tictactoe/" target="_blank" className="btn-portfolio btn">live site</a>
                 </div>
               </div>
             </div> 
@@ -69,7 +110,32 @@ var Applications = React.createClass({
 var Websites = React.createClass({
   render() {
     return (<div className="col-subpages">
-        <h2>here there will be websites huray</h2>
+        
+        <section className="col-desktop-narrow">    
+          <section className="wraper flex-center">
+            
+            <div className="wrapper flex-space-around">
+              <div className="col-520">
+                <div className="thumbnail img-portfolio">
+                  <a href="https://procreative.eu/" target="_blank">
+                    <img src="images/portfolio/procreative.png" alt="company website" />
+                  </a>
+                </div>
+              </div>
+              <div className="col-1-2 order-first">
+                <h3>ProCreative Website</h3>
+                <p>
+                  Company site based on Polymer. I worked on code, design, copy, cartoons, and movies.
+                </p>
+                <div className="btn-box">
+                  <a href="https://procreative.eu/" target="_blank" className="btn-portfolio btn">live site</a>
+                </div>
+              </div>
+            </div>
+              
+          </section>
+        </section>
+            
       </div>
     );
   }
